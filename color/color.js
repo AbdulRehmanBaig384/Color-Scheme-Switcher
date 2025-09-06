6de90cd0c5b4d69cd490b33c40cf32bfa69263a6
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-06 10:16:23 +0500

    Update color.js

--- color/color.js
+++ color/color.js
@@ -1,17 +1,70 @@
 const button=document.querySelectorAll('.button');
-const body=document.querySelector('body')
-button.forEach((function (buttons){
-    console.log(buttons)
-    buttons.addEventListener('click',function(e){
-        console.log(e)
-        if(e.target.id==='grey'){
-            body.style.backgroundColor=e.target.id}
-            if(e.target.id==='yellow'){
-                body.style.backgroundColor=e.target.id}
-                if(e.target.id==='purple'){
-                    body.style.backgroundColor=e.target.id}
-                    if(e.target.id==='aqua'){
-                        body.style.backgroundColor=e.target.id}
-    })
 
-}))
+const body=document.querySelector('body')
+
+button.forEach((function (buttons){
+    
+    console.log(buttons)
+    
+    buttons.addEventListener('click',function(e){
+        
+        console.log(e)
+        
+        if(e.target.id==='grey'){
+            
+      body.style.backgroundColor=e.target.id}
+        
+      if(e.target.id==='yellow'){
+                
+         body.style.backgroundColor=e.target.id}
+    
+      if(e.target.id==='purple'){
+                    
+          body.style.backgroundColor=e.target.id}
+        
+              if(e.target.id==='aqua'){
+                        
+              body.style.backgroundColor=e.target.id}
+    
+    }
+    )
+
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
